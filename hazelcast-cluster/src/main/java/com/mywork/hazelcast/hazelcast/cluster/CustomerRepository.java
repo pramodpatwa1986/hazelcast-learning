7219d67e078103cc9cc905edf0a19f5cb808aa6e
pramodpatwa11986 <pramod_patwa2003@yahoo.com>
2020-09-23 21:03:42 +0530

    incomplete jpa work

---
+++ hazelcast-cluster/src/main/java/com/mywork/hazelcast/hazelcast/cluster/CustomerRepository.java
@@ -0,0 +1,6 @@
+package com.mywork.hazelcast.hazelcast.cluster;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer,Long> {	
+}
--- hazelcast-cluster/src/main/java/com/mywork/hazelcast/hazelcast/cluster/HazelcastClusterApplication.java
+++ hazelcast-cluster/src/main/java/com/mywork/hazelcast/hazelcast/cluster/HazelcastClusterApplication.java
@@ -1,10 +1,14 @@
 package com.mywork.hazelcast.hazelcast.cluster;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.hazelcast.config.Config;
+import com.hazelcast.config.MapConfig;
+import com.hazelcast.config.MapStoreConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 
@@ -15,10 +19,32 @@ public class HazelcastClusterApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HazelcastClusterApplication.class, args);
 	}
-	
-	@Bean(name = "member1", destroyMethod="shutdown")
-	public HazelcastInstance createMember1() {
-		return Hazelcast.newHazelcastInstance();
+
+	@Bean(name = "member1", destroyMethod = "shutdown")
+	public HazelcastInstance createMember1(
+			@Qualifier("StoreNodeConfig") 
+			Config config) throws Exception{
+		return Hazelcast.newHazelcastInstance(config);
+	}
+
+	@Bean(name = "StoreNodeConfig")
+	public Config config(CustomerMapStore customerMapStore) throws Exception {
+
+		System.out.println("Config started");
+		Config config = new Config();
+
+		MapConfig customerMapConfig = new MapConfig();
+		
+		MapStoreConfig customerMapStoreConfig = new MapStoreConfig();
+		
+		customerMapStoreConfig.setImplementation(customerMapStore);
+		
+		customerMapConfig.setMapStoreConfig(customerMapStoreConfig);
+		customerMapConfig.setName("CUSTOMER");
+		
+		config.addMapConfig(customerMapConfig);
+
+		return config;
 	}
 
 }
